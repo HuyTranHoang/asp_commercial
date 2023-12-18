89412d3a9b85a25e930b79b7cb097b26b5d3190f
Huy Tran Hoang <nhokpio95@gmail.com>
2023-12-18 23:32:53 +0700

    Refactor imports in app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,22 @@
-import { BrowserAnimationsModule }        from '@angular/platform-browser/animations'
-import { BrowserModule }                  from '@angular/platform-browser'
-import { FormsModule }                    from '@angular/forms'
-import { NgModule }                       from '@angular/core'
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
-import { NgOptimizedImage }               from '@angular/common'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
 
-import { NavComponent }                   from './nav/nav.component'
-import { AppComponent }                   from './app.component'
-import { AppRoutingModule }               from './app-routing.module'
-import { SharedModule }                   from './_shared/shared.module'
+import { NavComponent } from './nav/nav.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './_shared/shared.module';
 import { APP_CONFIG, APP_SERVICE_CONFIG } from './_appconfig/appconfig.service';
-import { HomeRoutingModule } from './home/home-routing.module'
-import { ErrorInterceptor } from './_middleware/error.interceptor'
-import { NgxSpinnerModule } from 'ngx-spinner'
-import { BusyService } from './_shared/services/busy.service'
-import { LoadingInterceptor } from './_middleware/loading.interceptor'
-
-
+import { HomeRoutingModule } from './home/home-routing.module';
+import { ErrorInterceptor } from './_middleware/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_middleware/loading.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,13 +26,13 @@ import { LoadingInterceptor } from './_middleware/loading.interceptor'
     NgOptimizedImage,
     HomeRoutingModule,
     SharedModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
   ],
   providers: [
-    {provide: APP_SERVICE_CONFIG, useValue: APP_CONFIG},
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+    { provide: APP_SERVICE_CONFIG, useValue: APP_CONFIG },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
