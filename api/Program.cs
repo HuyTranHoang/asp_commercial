0e3bd2d06e76db6a5b2d36591869efdcc40084cd
Huy Tran Hoang <nhokpio95@gmail.com>
2023-12-23 03:45:23 +0700

    Refactor database migration and seeding in Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -31,22 +31,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    var userManager = services.GetRequiredService<UserManager<AppUser>>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedTypes(context);
-    await Seed.SeedBrands(context);
-    await Seed.SeedProducts(context);
-    await Seed.SeedUser(userManager);
-}
-catch (Exception ex)
-{
-    var logger = services.GetService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
-}
+app.MigrateAndSeedDatabase();
 
 app.Run();
